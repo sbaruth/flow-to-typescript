28eaafd4103faf9bd7f8b213680ca59bd337b5c0
Shaun Baruth <sbaruth@bloomberg.net>
2020-06-26 14:39:14 -0400

    Getting clone working.

--- clone_horseshoe.sh
+++ clone_horseshoe.sh
@@ -4,12 +4,13 @@
 . ./check_env.sh
 
 ###### CLONE
+cd ../
 echo cloning github.com/$user/$project
 git clone https://${user}:${token}@github.com/$user/$project
 cd horseshoe
 git checkout $branch
 
-yarn install 
+yarn install
 cd ios
 pod install
 cd ../..
